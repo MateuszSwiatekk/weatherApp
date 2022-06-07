b9ba4b243b78a5d9a1ae29a5b4f4eab3d9290186
Mateusz Świątek <252858@student.pwr.edu.pl>
2022-06-07 20:02:33 +0200

    Add images in description

--- app/src/main/java/com/example/weatherapp/Description.kt
+++ app/src/main/java/com/example/weatherapp/Description.kt
@@ -2,10 +2,7 @@ package com.example.weatherapp
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import android.widget.Toast.makeText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -19,12 +16,21 @@ class Description : AppCompatActivity() {
         setContentView(R.layout.activity_description)
 
         val plantImages= arrayOf(R.drawable.plant1,R.drawable.plant2,R.drawable.plant3,R.drawable.plant4,R.drawable.plant5,R.drawable.plant6,
-        R.drawable.plant7,R.drawable.plant8,R.drawable.plant8,R.drawable.plant9)
+        R.drawable.plant7,R.drawable.plant8,R.drawable.plant9,R.drawable.plant10,R.drawable.plant11,R.drawable.plant12,
+        R.drawable.plant13,R.drawable.plant14,R.drawable.plant15,R.drawable.plant16,R.drawable.plant17,R.drawable.plant18,R.drawable.plant19,
+        R.drawable.plant20,R.drawable.plant21,R.drawable.plant22,R.drawable.plant23,R.drawable.plant24,R.drawable.plant25,R.drawable.plant26,
+        R.drawable.plant27,R.drawable.plant28,R.drawable.plant29,R.drawable.plant30)
+
+        val image=findViewById<ImageView>(R.id.imageView8)
 
         val plant_array = intent.getStringArrayExtra("plant_id")
         val plant_id= plant_array?.get(0)
+        var img=plant_id?.toInt()
+        val type=plant_array?.get(2)
 
-
+            if (img != null) {
+                image.setImageResource(plantImages[img-1])
+            }
 
         var helper=DBHelper(applicationContext)
         var db=helper.readableDatabase
